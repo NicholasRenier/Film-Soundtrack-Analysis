c491f133bed241d9f754b10229bc3ed015273570
Nicholas Renier <nicholas.renier@outlook.com>
2018-12-07 00:05:15 +0000

    3.2

--- js/youre-so-cool.js
+++ js/youre-so-cool.js
@@ -13,8 +13,8 @@ if (content) {
 var defaultHeight = document.documentElement.clientHeight - contentHeight
 var defaultWidth = document.documentElement.clientWidth - 50
 options = {
-    pageHeight: defaultHeight * 300 / zoom,
-    pageWidth: defaultWidth * 70 / zoom,
+    pageHeight: defaultHeight * 800 / zoom,
+    pageWidth: defaultWidth * 100 / zoom,
     scale: zoom
 }
 vrvToolkit.setOptions(options)

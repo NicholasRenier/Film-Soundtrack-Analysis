594c48c79afa168365f78859ed901f8772b9e48f
Nicholas Renier <nicholas.renier@outlook.com>
2018-12-07 00:28:17 +0000

    3.6

--- js/youre-so-cool.js
+++ js/youre-so-cool.js
@@ -13,7 +13,7 @@ if (content) {
 var defaultHeight = document.documentElement.clientHeight - contentHeight
 var defaultWidth = document.documentElement.clientWidth - 50
 options = {
-    pageHeight: defaultHeight * 200 / zoom,
+    pageHeight: defaultHeight * 100 / zoom,
     pageWidth: defaultWidth * 80 / zoom,
     scale: zoom
 }

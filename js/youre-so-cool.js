67c04cdd4b98e50efcc61439b98515bd8293fc38
NicholasRenier <45599482+NicholasRenier@users.noreply.github.com>
2018-12-07 03:49:01 +0000

    Add files via upload

---
+++ js/youre-so-cool.js
@@ -0,0 +1,23 @@
+/* This script loads the MEI file via HTTP request */
+
+/* Start Verovio */
+var vrvToolkit = new verovio.toolkit()
+
+/* Setup Verovio - adjust the options below */
+var zoom = 50
+var content = document.getElementById('content')
+var contentHeight = 0
+if (content) {
+  contentHeight = content.offsetHeight
+}
+var defaultHeight = document.documentElement.clientHeight - contentHeight
+var defaultWidth = document.documentElement.clientWidth - 50
+options = {
+    pageHeight: defaultHeight * 220 / zoom,
+    pageWidth: defaultWidth * 80 / zoom,
+    scale: zoom
+}
+vrvToolkit.setOptions(options)
+
+/* Show the score */
+showScore('score', 'data/youresocool.mei') // <-- CHANGE THIS TO YOUR FILE

c6ca6f69d3375ea4d5cdf7985d32b7d054aa96ec
NicholasRenier <45599482+NicholasRenier@users.noreply.github.com>
2018-12-07 03:47:18 +0000

    Delete clickExample.js

--- js/clickExample.js
+++
@@ -1,7 +0,0 @@
-function registerClick(listOfIds) {
-  for (var i=0; i < listOfIds.length; i++) {
-    document.querySelector('#'+listOfIds[i]).addEventListener('click', function () {
-      // DO SOMETHING ON EACH CLICK
-    })
-  }
-}
